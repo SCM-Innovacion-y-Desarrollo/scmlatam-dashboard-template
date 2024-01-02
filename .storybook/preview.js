d5dc5457b549736274b1907d6fbaf5bbc9752f22
Nicolas Rivera <43177162+elwuapo@users.noreply.github.com>
2024-01-01 21:37:56 -0300

    addon-themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
-/** @type { import('@storybook/react').Preview } */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom Material UI themes */
+
+import { lightTheme, darkTheme } from '../src/styles/theme.jsx';
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,6 +20,17 @@ const preview = {
       },
     },
   },
+
+  decorators: [withThemeFromJSXProvider({
+    GlobalStyles: CssBaseline,
+    Provider: ThemeProvider,
+    themes: {
+      // Provide your custom themes here
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+  })]
 };
 
 export default preview;
